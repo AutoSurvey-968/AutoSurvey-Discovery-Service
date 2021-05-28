8a133a28e379e558deb1eddf728bb086ac74d7a8
DuskDaleSpider <dakota.ray.clark@gmail.com>
2021-05-28 14:28:05 -0400

    added run-docker sh script that jenkins will use

---
+++ docker/run-docker.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Check if autosurvey-network network exists
+if [ -z $(docker network ls -q -f name=autosurvey-network) ]; then
+    docker network create autosurvey-network
+fi
+
+# rm discovery-service container if it exists
+if [ -z $(docker container ls -aqf name=discovery-service) ]; then 
+    docker container rm discovery-service
+fi
+
+#start discovery-service container
+docker container run --name discovery-service --network autosurvey-network \
+    -p 8761:8761 autosurvey/discovery-service
